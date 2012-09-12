6b69d735c196c0c41cff4cbfe2a3c887f8e4698b
David Lawson <dlawson@xylemdigital.com>
2012-09-12 17:28:52 -0600

    pull footer out of theme settings to allow for better translation.
    trim footer nav args.

--- themes/donor-alliance/footer/footer-default.php
+++ themes/donor-alliance/footer/footer-default.php
@@ -26,25 +26,14 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 			<div class="in">
 				<div id="footer-interior">
 					<div class="body">
-							<p class="copyright"><?php echo cfct_get_option('cfct_copyright'); ?></p>
+							<p class="copyright">&copy;2012 Donor Alliance, Inc. <?php _e('All Rights Reserved'); ?></p>
 							<?php
 							$args = array(
 								'theme_location'  => 'nav-footer',
-								// 'menu'            => ,  	
 								'container'       => 'nav', 
-								// 'container_class' => 'menu-{menu slug}-container', 
 								'container_id'    => 'nav-footer',
 								'menu_class'      => 'nav', 
-								// 'menu_id'         => ,
 								'echo'            => true,
-								// 'fallback_cb'     => 'wp_page_menu',
-								// 'before'          => ,
-								// 'after'           => ,
-								// 'link_before'     => ,
-								// 'link_after'      => ,
-								// 'items_wrap'      => '<ul id=\"%1$s\" class=\"%2$s\">%3$s</ul>',
-								// 'depth'           => 0,
-								// 'walker'          => 
 							);
 							wp_nav_menu( $args );
 							cfct_form('newsletter');
